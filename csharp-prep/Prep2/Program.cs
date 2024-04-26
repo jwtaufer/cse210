155ad1a29341971ae7df99e6062179533007589d
Jake Taufer <jwtaufer@gmail.com>
2024-04-26 12:40:40 -0600

    Finished Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string strGrade = Console.ReadLine();
+        int intGrade = int.Parse(strGrade);
+
+        string letter = "";
+
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+        else if (intGrade < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            letter = "error";
+        }
+
+        Console.Write($"Your grade is: {letter}");
     }
 }
