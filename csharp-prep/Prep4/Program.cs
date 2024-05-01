f75ab42530dd349923228e73c40c733dc18090a0
Jake Taufer <jwtaufer@gmail.com>
2024-05-01 17:21:53 -0600

    Finished Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = 1;
+        int iteration = 0;
+        int sum = 0;
+        int average = 0;
+        int maximum = 0;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+                iteration++;
+                sum += number;
+
+                if (number > maximum)
+                {
+                    maximum = number;
+                }
+            }
+        }
+
+        average = sum/iteration;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
     }
 }
