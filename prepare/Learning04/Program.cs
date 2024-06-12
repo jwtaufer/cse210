5bfc25528e8abc71345ab5aa0f67944b7cbd3fbb
Jake Taufer <jwtaufer@gmail.com>
2024-06-12 13:18:33 -0600

    Commented out unecessary code.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("Jake", "Math");
-        Console.WriteLine(assignment.GetSummary());
+        // Assignment assignment = new Assignment("Jake", "Math");
+        // Console.WriteLine(assignment.GetSummary());
 
-        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
-        Console.WriteLine(mathAssignment.GetSummary());
-        Console.WriteLine(mathAssignment.GetHomeworkList());
+        // MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        // Console.WriteLine(mathAssignment.GetSummary());
+        // Console.WriteLine(mathAssignment.GetHomeworkList());
 
         WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(writingAssignment.GetSummary());
